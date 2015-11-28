a9e55e26337cedc07c4de86f958bdc12f5be058f
PYARIA <jadetong@gmail.com>
2015-11-27 21:03:09 -0800

    Answers controller some CRUD logic, new, edit, _form views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
 
-  devise_for :users
   # root to: "home#index"
   resources :drill_groups
-  
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
+  resources :answers
+
 end
