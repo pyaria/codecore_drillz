0aa413eb847c918560541d154481ae7795abee88
Ian Stewart <iastewar555@gmail.com>
2015-11-27 23:00:20 -0800

    added bootstrap and made drills work

--- Gemfile
+++ Gemfile
@@ -5,6 +5,9 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 gem 'devise', '~> 3.5', '>= 3.5.2'
+gem "bootstrap-sass"
+gem "font-awesome-rails"
+
 
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
--- app/controllers/drills_controller.rb
+++ app/controllers/drills_controller.rb
@@ -1,19 +1,21 @@
 class DrillsController < ApplicationController
   #before_action :authenticate_user, only: [:create, :destroy, :edit, :update]
+  before_action :find_drill_group, only: [:create, :show, :edit, :update, :destroy]
+  before_action :find_drill, only: [:show, :edit, :update, :destroy]
+
 
   def create
-    drill_params = params.require(:drill).permit(:body)
-    @drill_group = DrillGroup.find params[:drill_group_id]
+    @dg = DrillGroup.find params[:drill_group_id]
     @drill = Drill.new drill_params
-    @drill.drill_group = @drill_group
+    @drill.drill_group = @dg
     #@drill.user = current_user
 
     respond_to do |format|
       if @drill.save
-        format.html { redirect_to drill_group_path(@drill_group), notice: "Drill created succussfully!" }
+        format.html { redirect_to drill_group_path(@dg), notice: "Drill created succussfully!" }
         format.js { render :create_success }
       else
-        @drills = @drill_group.drills.order(created_at: :desc)
+        @drills = @dg.drills.order(created_at: :desc)
         format.html { render "drill_groups/show" }
         format.js { render :create_failure }
       end
@@ -21,25 +23,23 @@ class DrillsController < ApplicationController
   end
 
   def edit
-    @drill_group = DrillGroup.find_by_id params[:drill_group_id]
+    @dg = DrillGroup.find_by_id params[:drill_group_id]
     @drill = Drill.find_by_id params[:id]
     respond_to do |format|
-      redirect_to drill_group_path(@drill_group), alert: "Access denied." and return unless can? :edit, @drill
+      #redirect_to drill_group_path(@dg), alert: "Access denied." and return unless can? :edit, @drill
       format.js { render }
     end
   end
 
   def update
-    drill_params = params.require(:drill).permit(:name, :description)
-    @drill_group = DrillGroup.find params[:drill_group_id]
+    @dg = DrillGroup.find params[:drill_group_id]
     @drill = Drill.find params[:id]
     respond_to do |format|
       if @drill.update(drill_params)
-        format.html { redirect_to drill_group_path(@drill_group), notice: "Drill updated!" }
+        format.html { redirect_to drill_group_path(@dg), notice: "Drill updated!" }
         format.js { render :update_success }
       else
-        @drills = @drill_group.drills.order(created_at: :desc)
-        @assets = @drill_group.assets.order(created_at: :desc)
+        @drills = @dg.drills.order(created_at: :desc)
         format.html { render "drill_groups/show" }
         format.js { render :update_failure }
       end
@@ -50,9 +50,9 @@ class DrillsController < ApplicationController
     @drill = Drill.find_by_id params[:id]
     respond_to do |format|
       if @drill
-        redirect_to drill_group_path(params[:drill_group_id]), alert: "Access denied." and return unless can? :destroy, @drill
+        #redirect_to drill_group_path(params[:drill_group_id]), alert: "Access denied." and return unless can? :destroy, @drill
         @drill.destroy
-        format.html { redirect_to drill_group_path(drill.drill_group), notice: "Drill deleted" }
+        format.html { redirect_to drill_group_path(@drill.drill_group), notice: "Drill deleted" }
         format.js { render }
       else
         format.html { redirect_to drill_group_path(params[:drill_group_id]), notice: "Drill already deleted" }
@@ -61,8 +61,22 @@ class DrillsController < ApplicationController
   end
 
   def show
+    @dg = DrillGroup.find_by_id params[:drill_group_id]
     @drill = Drill.find_by_id params[:id]
   end
 
+  private
+
+  def drill_params
+    params.require(:drill).permit(:name, :description)
+  end
+
+  def find_drill_group
+    @dg = DrillGroup.find params[:drill_group_id]
+  end
+
+  def find_drill
+    @drill = Drill.find params[:id]
+  end
 
 end
