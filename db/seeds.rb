f6ac74d02c9972491a91dbc658fc0dc258b8e22c
Hongyeung <manhongyeung@gmail.com>
2015-11-28 14:26:56 -0800

    Added seeds with answers associated with drills. Also, added an image in assets pipeline for background.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,10 @@
-10.times do
-  Drill.create({name: Faker::Company.bs,
-                description:  Faker::Lorem.paragraph })
-end
 
 10.times do
   DrillGroup.create({name: Faker::Hacker.noun})
+end
+
+10.times do
+  answer = Answer.create({description: Faker::Hacker.say_something_smart})
+  drill = Drill.create({name: Faker::Company.bs,
+                description:  Faker::Lorem.paragraph, answer_id: answer.id })
 end
