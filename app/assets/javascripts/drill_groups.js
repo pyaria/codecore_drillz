5057b9b15e19c733c2f82a733b99933a3dfd551d
PYARIA <jadetong@gmail.com>
2015-11-29 15:13:50 -0800

    cleaned up comments on previous

--- app/assets/javascripts/drill_groups.js
+++ app/assets/javascripts/drill_groups.js
@@ -19,12 +19,10 @@ $(function(){
       })
       .autocomplete({
         source: function( request, response ) {
-          // delegate back to autocomplete, but extract the last term
           response( $.ui.autocomplete.filter(
             data, extractLast( request.term ) ) );
         },
         focus: function() {
-          // prevent value inserted on focus
           return false;
         },
         select: function( event, ui ) {
@@ -33,15 +31,6 @@ $(function(){
           terms.push(ui.item.value);
           terms.push("");
           $('#drill_group_category_ids').val(terms.join(", "));
-          // var terms = data.split(ui.item.value);
-          // var terms = this.split(value );
-          // // remove the current input
-          // terms.pop();
-          // // add the selected item
-          // terms.push( ui.item.value );
-          // // add placeholder to get the comma-and-space at the end
-          // terms.push( "" );
-          // this.value = terms.join( ", " );
           return false;
         }
       });
