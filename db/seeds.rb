51f6e84ef75fb63798c98a354877928da925e945
Zaid Nizami <nizami.zaid@gmail.com>
2015-11-29 20:10:24 -0800

    assocations added to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,89 +1,47 @@
-# 10.times do
-#
-#   DrillGroup.create!({name: Faker::Hacker.noun})
-# end
-#
-# 10.times do
-# 	Drill.create({name: Faker::Company.bs,
-#                 description:  Faker::Lorem.paragraph,
-#                 points: 10 })
-#
-# #
-#   DrillGroup.create!({name: Faker::Hacker.noun})
-#
-#   user = User.find(1)
-#   dg = DrillGroup.create({name: Faker::Hacker.noun,
-#                           description: Faker::Hacker.say_something_smart,
-#                           user_id: user.id
-#                           })
-#   answer = Answer.create({description: Faker::Hacker.say_something_smart})
-#   drill = Drill.create({name: Faker::Company.bs,
-#                         description:  Faker::Lorem.paragraph, answer_id: answer.id,
-#                         drill_group_id: dg.id })
-#
-# end
-#
-# 10.times do
-#   User.create({ first_name:   Faker::Name.first_name,
-#                 last_name:    Faker::Name.last_name,
-#                 email:        Faker::Internet.email,
-#                 password:      Faker::Internet.password
-#                   })
-# end
-#
-# 10.times do
-#
-#   user  = User.create(
-#                 {first_name: Faker::Name.first_name,
-#                 last_name:  Faker::Name.last_name,
-#                 email:      Faker::Internet.email,
-#                 password:   Faker::Internet.password })
-# end
+admin  = User.create( 
+	:first_name => "Johnathan", 
+	:last_name	=> "Macintosh",  
+	:email 			=> "user122f2345@admin.com", 
+	:password 	=> "password",
+	:admin      => true,
+	:password_confirmation => "password")
 
+non_admin = User.create( 
+	:first_name => "Jim",
+	:last_name 	=> "Walker",
+	:email 			=> "user6@admin.com",
+	:password 	=> "password",
+	:password_confirmation => "password")
+	
+#non_admin.drills.push(Drill.new(name: "Personless", description: "Super fun, you should try it"))
+non_admin.save
+
+non_admin = User.create!(
+  :first_name => "Jenny",
+  :last_name 	=> "Johnson",
+  :email 			=> "user624534543@admin.com",
+  :password 	=> "password",
+  :password_confirmation => "password")
+ 
+		
 10.times do
-
-  DrillGroup.create!({name: Faker::Hacker.noun})
+ DrillGroup.create!({name: Faker::Hacker.noun,
+  									 user_id: admin})
 end
 
+DrillGroup.all.each do |dg|
 10.times do
-	Drill.create!({name: Faker::Company.bs,
-                description:  Faker::Lorem.paragraph, 
-                points: 1})
-
-  DrillGroup.create!({name: Faker::Hacker.noun})
-  user = User.find(1) 
-  dg 	= DrillGroup.create!({name: Faker::Hacker.noun,
-                          description: Faker::Hacker.say_something_smart,
-                          user_id: user.id
-                          })
-
-
-  answer = Answer.create!({description: Faker::Hacker.say_something_smart})
-  drill = Drill.create!({name: Faker::Company.bs,
-                        description:  Faker::Lorem.paragraph, answer_id: answer.id,
-                        points: 1,
-                        drill_group_id: dg.id })
-
+	Drill.create!({name: 				 Faker::Company.bs,
+                 description:  Faker::Lorem.paragraph, 
+                 points: 1,
+                 user_id: admin,
+                 drill_group_id: dg
+                 })
+ end
 end
+	
 
-
-10.times do
-  User.create(
-  							{ first_name:   Faker::Name.first_name,
-                last_name:    Faker::Name.last_name,
-                email:        Faker::Internet.email,
-                password:      Faker::Internet.password })
-end
-
-10.times do
-
-  user  = User.create(
-                {first_name: Faker::Name.first_name,
-                last_name:  Faker::Name.last_name,
-                email:      Faker::Internet.email,
-                password:   Faker::Internet.password })
-end
-
+  #answer = Answer.create!({ description: Faker::Hacker.say_something_smart})
 
 badges = ["Ruby Lover", "Rails Routes", "Javascript Master", "Rails Newbie"]
 #badges.sort! {|a, b| a <=> b}
@@ -92,65 +50,9 @@ badges.each do |badge|
   Badge.create(name: badge)
 end
 
-# 10.times do
-# 	answer = Answer.create!({description: Faker::Hacker.say_something_smart})
-# 	Drill.create!({name: Faker::Company.bs,
-#                	 description:  Faker::Lorem.paragraph, answer_id: answer.id })
-# end
-#
-#
-#
-# admin = User.create!(
-# 	:first_name => "master",
-# 	:last_name	=> "bot",
-# 	:email 			=> "user1222345@admin.com",
-# 	:password 	=> "password",
-# 	:password_confirmation => "password")
-#
-# non_admin = User.create(
-# 	:first_name => "person",
-#   :last_name 	=> "human",
-#   :email 			=> "user6@admin.com",
-#   :password 	=> "password",
-#   :password_confirmation => "password")
-#
-# non_admin.drills.push(Drill.new(name: "Personless", description: "Super fun, you should try it"))
-# non_admin.save
-#
-# non_admin = User.create!(
-#   :first_name => "person",
-#   :last_name 	=> "human",
-#   :email 			=> "user624534543@admin.com",
-#   :password 	=> "password",
-#   :password_confirmation => "password")
 
 10.times do
   Category.create(name: Faker::Hacker.noun)
 end
 
-admin = User.create( 
-	:first_name => "master", 
-	:last_name	=> "bot",  
-	:email 			=> "user1222345@admin.com", 
-	:password 	=> "password",
-	:admin      => true,
-	:password_confirmation => "password")
-
-	
-
-non_admin = User.create( :first_name => "person",
-:last_name 	=> "human",
-:email 			=> "user6@admin.com",
-:password 	=> "password",
-:password_confirmation => "password")
-
-#non_admin.drills.push(Drill.new(name: "Personless", description: "Super fun, you should try it"))
-non_admin.save
-
-non_admin = User.create!(
-  :first_name => "person",
-  :last_name 	=> "human",
-  :email 			=> "user624534543@admin.com",
-  :password 	=> "password",
-  :password_confirmation => "password")
 
